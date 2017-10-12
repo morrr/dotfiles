1f706720a73e5095124fae24d038c4d5099389c3
morrr <morrr2154@gmail.com>
2017-10-12 13:18:38 +0900

    add status-bar config on tmux

---
+++ bin/git-echo-username-and-email.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+# https://qiita.com/koara-local/items/940ce66e2ecd8e4d8582
+
+cd `tmux display-message -p -F "#{pane_current_path}"`
+user_name=`git config --get user.name`
+email_address=`git config --get user.email`
+
+echo "[${user_name} | ${email_address}]"
