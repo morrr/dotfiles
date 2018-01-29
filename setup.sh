7b4e0246c85ff04aaa55b476624f8fb7b5b7101f
Yuta Morita <morrr2154@gmail.com>
2018-01-29 21:28:55 +0900

    add dock config, dein config on setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+#
+# dock
+#
+sudo defaults write com.apple.Dock showhidden -boolean true
+killall Dock
+
+#
+# vim
+#
+mkdir -p ~/Workspace/github.com/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim.git \
+    ~/Workspace/github.com/Shougo/dein.vim
+
+#
+# rc
+#
 ln -sf ~/Workspace/github.com/morrr/dotfiles/.vim ~/.vim
 ln -sf ~/Workspace/github.com/morrr/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/Workspace/github.com/morrr/dotfiles/.gvimrc ~/.gvimrc
