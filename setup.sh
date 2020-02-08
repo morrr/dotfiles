52c657dcf9efd401dbd374ed3492c9ec21293afe
Yuta Morita <morrr2154@gmail.com>
2020-02-09 00:49:19 +0900

    vscodeのvimバインドでキーリピートが効くよう修正 + フォントのインストール

--- setup.sh
+++ setup.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+#
+# Env
+#
 REPO_BASE_DIR=~/Workspace/github.com
 # spaceを含む場合はフルパスでないと正しく展開されない...？
 VSCODE_BASE_DIR="/Users/morrr/Library/Application Support/Code/User"
 
 #
-# dock
+# Font
+#
+brew tap homebrew/cask-fonts && brew cask install font-fira-code
+brew cask install font-inconsolata
+
+#
+# Dock
 #
 sudo defaults write com.apple.Dock showhidden -boolean true
 killall Dock
 
 #
-# vim
-#
-mkdir -p $REPO_BASE_DIR/Shougo/dein.vim
-git clone https://github.com/Shougo/dein.vim.git $REPO_BASE_DIR/Shougo/dein.vim
-
-#
-# symlink config files
+# Symlink config files
 #
 ln -sf $REPO_BASE_DIR/morrr/dotfiles/.vim ~/.vim
 ln -sf $REPO_BASE_DIR/morrr/dotfiles/.vimrc ~/.vimrc
@@ -32,11 +35,23 @@ ln -sf $REPO_BASE_DIR/morrr/dotfiles/vscode.settings.json "$VSCODE_BASE_DIR/sett
 ln -sf $REPO_BASE_DIR/morrr/dotfiles/vscode.keybindings.json "$VSCODE_BASE_DIR/keybindings.json"
 
 #
-# install vscode extensions
+# Vim
 #
+mkdir -p $REPO_BASE_DIR/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim.git $REPO_BASE_DIR/Shougo/dein.vim
+
+
+#
+# Visual Studio Code
+#
+# install extensions
 # output: $code --list-extensions > vscode.extensions
 cat $REPO_BASE_DIR/morrr/dotfiles/vscode.extensions | while read line
 do
  code --install-extension $line
 done
+
+# enable key repeat on macos
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
 
